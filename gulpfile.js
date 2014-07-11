9dbaf62e0c259ecef672aaaca7f1e4ede7a48b0d
ratsam <m4spam@gmail.com>
2014-07-12 03:36:24 +0400

    Restore 'gulp install' task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var sh = require('shelljs');
 var minifyCss = require('gulp-minify-css');
 
 var livereload = require('gulp-livereload');
@@ -59,3 +60,23 @@ gulp.task('clean', function () {
 gulp.task('build', ['images', 'fonts', 'configs', 'usemin']);
 
 gulp.task('default', ['build', 'server-dist']);
+
+gulp.task('install', ['git-check'], function() {
+    return bower.commands.install()
+        .on('log', function(data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+gulp.task('git-check', function(done) {
+    if (!sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
+});
