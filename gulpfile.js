f518a6faf317cc7bba649a77682251a3edf10ff6
WonderBeat <borov.htid@gmail.com>
2014-09-24 13:27:10 +0400

    livareload

--- gulpfile.js
+++ gulpfile.js
@@ -21,34 +21,49 @@ var rework = require('gulp-rework');
 var assets = require('rework-assets');
 var reworkSuit = require('rework-suit');
 var karma = require('karma').server;
+var injectReload = require('gulp-inject-reload');
+var noop = require('gulp-util').noop;
 
 var paths = {
   'dist': 'www'
 };
 
-gulp.task('usemin', function() {
-  var reworkCssPipe = rework(
-      assets({ src: 'app/components', dest: paths.dist + '/assets', prefix: '../assets/' }),
-      reworkSuit());
-  var useminPipe = function() {
-    return usemin({
-      css: [minifyCss(), reworkCssPipe, 'concat'],
-      scss: [sass(), reworkCssPipe],
-      less: [sourcemaps.init(), less(), sourcemaps.write()],
-      html: [minifyHTML({empty: true})],      
-      js: [uglify({mangle: false})]
-    });
-  };
+var useminPipe = function(minify) {
+    var reworkCssPipe = rework(
+        assets({ src: 'app/components', dest: paths.dist + '/assets', prefix: '../assets/' }),
+        reworkSuit());
+    var useminPipe = function() {
+      return usemin({
+        css: [minify ? minifyCss() : noop(), reworkCssPipe, 'concat'],
+        scss: [sass(), reworkCssPipe],
+        less: [sourcemaps.init(), less(), sourcemaps.write()],
+        html: [minify ? minifyHTML({empty: true}) : noop()],
+        js: [minify ? uglify({mangle: false}) : noop()]
+      });
+    };
+    return useminPipe();
+}
 
-  gulp.src(['app/*.html']).pipe(useminPipe()).pipe(gulp.dest(paths.dist));
-  gulp.src(['app/templates/*.html']).pipe(useminPipe()).pipe(gulp.dest(paths.dist + '/templates'));
-});
+var compile = function(minify, liveReload) {
+  gulp.src(['app/*.html'])
+    .pipe(liveReload ? injectReload() : noop())
+    .pipe(liveReload ? livereload() : noop())
+    .pipe(useminPipe(minify))
+    .pipe(gulp.dest(paths.dist));
+  gulp.src(['app/templates/*.html'])
+    .pipe(useminPipe(minify))
+    .pipe(liveReload ? livereload() : noop())
+    .pipe(gulp.dest(paths.dist + '/templates'));
+}
+
+gulp.task('usemin', function() { compile(true, false); });
+gulp.task('nomin', function() { compile(false, true); });
 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*', ['usemin']);
+  gulp.watch('app/**/*', ['nomin']);
 });
 
-gulp.task('ripple', ['watch'], function() {
+gulp.task('ripple', ['nomin', 'watch'], function() {
   ripple.emulate.start({ port: 4400 });
   open('http://localhost:4400/?enableripple=cordova-3.0.0');
 }); 
@@ -58,11 +73,6 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('build', ['usemin']);
-
-gulp.task('test', ['build'], function(done) {
-  console.log('No tests yet');
-  done();
-});
 
 gulp.task('default', ['build', 'ripple']);
 
